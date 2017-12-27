475ee9dd1cfda4332ac0673f8d9a5d4b1571a93e
YuryKoropenko <yurykoropenko@gmail.com>
2017-12-27 10:47:11 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	
+	// валидация телефона
+    var phone_vop = {"mask":"+7(999)999-99-99"};
+    jQuery("#phone-01, #phone-02, #phone-03, #phone-04, #phone-05").inputmask(phone_vop);
+	
+	
+	// Выбор иконок в блоке "Рядом с комплексом"
+	$('.icon-block img').click(function(){
+		$('.icon-block img').each(function(){
+			var id = $(this).data('id');
+			$(this).attr('src', 'img/gallery/icons/0'+id+'.png');
+		});	
+		var id = $(this).data('id');
+		$(this).attr('src', 'img/gallery/icons/0'+id+'w.png');
+	});
+	
+	
+});
