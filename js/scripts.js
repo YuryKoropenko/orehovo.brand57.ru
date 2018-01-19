4fdc5ce109f29ddd9a6150294ac934636cd9178a
YuryKoropenko <yurykoropenko@gmail.com>
2018-01-19 12:15:57 +0200

    new-slider

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,34 @@ $(document).ready(function(){
 	$('.green-btn-phone').fancybox();
 
 	$('.p-tabs').lightTabs();
+
+
+
+(function(){
+  // setup your carousels as you normally would using JS
+  // or via data attributes according to the documentation
+  // https://getbootstrap.com/javascript/#carousel
+  $('#carouselExampleControls-4').carousel({ interval: 5000 });
+}());
+
+(function(){
+  $('#carouselExampleControls-4 .carousel-item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<4;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
 });
+
